594094d6f8eb8e3e096e97a41fd9e42d45b564d1
egor5q <31898567+egor5q@users.noreply.github.com>
2018-03-05 17:27:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -54,7 +54,7 @@ def m(m):
     bot.send_message(441399484, m.chat.id)
     if m.chat.id not in info.lobby.game:
       if m.chat.id==-1001117073605:
-        t=threading.Timer(1200, del2, args=[chats])
+        t=threading.Timer(1200, del2, args=[m.chat.id])
         t.start()
         Keyboard=types.InlineKeyboardMarkup()
         info.lobby.game.update(createroom(m.chat.id))
