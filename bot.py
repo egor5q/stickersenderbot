3dc1be224a285aa19ed71b960928587d9edc9f85
egor5q <31898567+egor5q@users.noreply.github.com>
2018-03-05 16:24:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,12 @@ randlist=['Шальная Императрица', 'Равен', 'Котейка
 @bot.callback_query_handler(func=lambda call:True)
 def inline(call):
     if call.data=='join':
+      z=0
       for chats in info.lobby.game:
+        if call.from_user.id in info.lobby.game[chats]['players']:
+            z+=1
+      for chats in info.lobby.game:    
+       if z==0:
         if call.from_user.id not in info.lobby.game[chats]['players']:
             if len(info.lobby.game[chats]['players'])<8:
               info.lobby.game[chats]['players'].update(createuser(call.from_user.id, chats))
