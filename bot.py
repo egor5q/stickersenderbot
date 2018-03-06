7e46b323d8e39e96380556f35d9f7367e173236f
egor5q <31898567+egor5q@users.noreply.github.com>
2018-03-06 23:47:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,126 +11,12 @@ from telebot import types
 from emoji import emojize
 token = os.environ['TELEGRAM_TOKEN']
 bot = telebot.TeleBot(token)
-randlist=['Шальная Императрица', 'Равен', 'Котейка', 'Артем', 'Возбужденный Самец', 'Писюк', 'Веган', 'Пйос', 'Большой Банан', 'Мявс', 'Клубничка', 'Гей Воробей', 'Енотик']
-@bot.callback_query_handler(func=lambda call:True)
-def inline(call):
-    if call.data=='join':        
-      try:
-        if call.from_user.id not in info.lobby.game[call.message.chat.id]['players']:
-              z=0
-              for ids in info.lobby.game:
-                  if call.from_user.id in info.lobby.game[ids]['players']:
-                    z+=1    
-              if z==0:
-               if len(info.lobby.game[call.message.chat.id]['players'])<12:
-                  info.lobby.game[call.message.chat.id]['players'].update(createuser(call.from_user.id, call.message.chat.id))
-                  bot.send_message(call.message.chat.id, 'Аноним присоединился!')
-                  info.lobby.alreadyplay.append(call.from_user.id)
-               if len(info.lobby.game[call.message.chat.id]['players'])>11:
-                bot.send_message(call.message.chat.id, 'Набор окончен!')
-                begin(call.message.chat.id)
-      except:
-        pass
-                                  
 
 
-def del2(id):
-    try:
-      bot.send_message(id, '25 минут прошло! Вирт остановлен!')
-      del info.lobby.game[id]
-    except:
-      pass
+@bot.message_handler(commands=['chlen'])
+def c(m):
+    bot.send_message(m.chat.id, 'Да да да, я работаю, отъебитесь')
 
-def delplayer(id, id2):
-    del info.lobby.game[id]['players'][id2]
-    info.lobby.game[id]['nicks'].remove(info.lobby.game[id]['players'][id2]['nick'])
-            
-def deleter(id):
-  try:
-    del info.lobby.game[id]
-  except:
-    pass
-    
-@bot.message_handler(commands=['stop'])
-def s(m):
-  for ids in info.lobby.game:
-    if m.from_user.id in info.lobby.game[ids]['players']:
-        bot.send_message(ids, 'Аноним вышел!')
-        t=threading.Timer(0.1, delplayer, args=[ids, m.from_user.id])
-        t.start()
-
-@bot.message_handler(commands=['lobby'])
-def m(m):
-    if m.chat.id not in info.lobby.game:
-        info.lobby.game.update(createroom(m.chat.id))
-        t=threading.Timer(1500, del2, args=[m.chat.id])
-        t.start()
-        info.lobby.game[m.chat.id]['timer']=t
-        bot.send_message(441399484, 'Вирт начался где-то!')
-        Keyboard=types.InlineKeyboardMarkup()          
-        Keyboard.add(types.InlineKeyboardButton(text='Тык', callback_data='join'))
-        info.lobby.game[m.chat.id]['startm']=bot.send_message(m.chat.id, 'Начинаем! жмите на кнопку, чтобы присоединиться', reply_markup=Keyboard)
-    else:
-      try:
-        bot.reply_to(info.lobby.game[m.chat.id]['startm'], 'Вирт уже идёт!')
-      except:
-        pass
-
-def namechoice(id):
-    x=random.choice(randlist)
-    while x in info.lobby.game[id]['nicks']:
-        x=random.choice(randlist)
-    info.lobby.game[id]['nicks'].append(x)
-    return x
-        
-        
-def begin(id):
-    for ids in info.lobby.game[id]['players']:
-      try:
-        bot.send_message(ids, 'Пишите сюда что то')
-      except:
-        bot.send_message(id, 'Какой то пидорас не открыл диалог с ботом!')
-    
-    
-@bot.message_handler(content_types=['text'])
-def h(m):
-    for ids in info.lobby.game:
-        if m.from_user.id in info.lobby.game[ids]['players']:
-          if m.chat.id>0:
-            try:
-              bot.send_message(ids, info.lobby.game[ids]['players'][m.from_user.id]['name']+':\n'+m.text)
-              info.lobby.game[ids]['timer'].cancel()
-              t=threading.Timer(1500, del2, args=[ids])
-              t.start()
-              info.lobby.game[ids]['timer']=t
-            except:
-                pass
-            try:
-                for idd in info.lobby.game[ids]['players']:
-                    if m.from_user.id!=idd:
-                      bot.send_message(idd, info.lobby.game[ids]['players'][m.from_user.id]['name']+':\n'+m.text)
-            except:
-                pass
-
-    
-def createroom(id):
-  return{id:{
-      'nicks':[],
-      'startm':None,
-      'timer':None,
-    'players':{
-    }
-     }
-      }   
-        
-def createuser(id, chatid):
-    return{id:{
-           'name':namechoice(chatid)
-          }
-          }
-       
-       
-       
        
 if __name__ == '__main__':
   bot.polling(none_stop=True)
