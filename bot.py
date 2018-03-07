a30eb61b5e5684faea3fd75f897ac752a700af31
egor5q <31898567+egor5q@users.noreply.github.com>
2018-03-07 15:03:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ token = os.environ['TELEGRAM_TOKEN']
 bot = telebot.TeleBot(token)
 pisuks=0
 spisok=['Пиздец', 'Бляяяяяя', 'Дороу', 'Кто будет сосать?']
-
+spisok2=[]
 @bot.message_handler(commands=['chlen'])
 def c(m):
     bot.send_message(m.chat.id, 'Да да да, я работаю, отъебитесь')
@@ -33,9 +33,14 @@ def textm(m):
     if pisuks==1:
         print('1')
         if 'п' in p and 'и' in p and 'д' in p and 'р' in p and len(p)<250:
-          print('2')
-          bot.send_message(m.chat.id, 'Нахуй иди')
-          pisuks=0
+            for x in p:
+                if x not in spisok2:
+                    spisok2.append(x)
+            if len(spisok2)<15:                
+              print('2')
+              bot.send_message(m.chat.id, 'Нахуй иди')
+              pisuks=0
+            spisok2.clear()
     z=random.randint(1, 100)
     if z==1:
         speach=random.choice(spisok)
@@ -44,9 +49,14 @@ def textm(m):
         pisuks=1
         print('3')
     if 'п' in p and 'с' in p and 'ю' in p and 'к' in p and 'д' in p and 'р' in p and len(p)<250:
-        print('4')
-        pisuks=0
-        bot.send_message(m.chat.id, 'Нахуй иди')
+        for x in p:
+          if x not in spisok2:
+             spisok2.append(x)
+        if len(spisok2)<15:           
+          print('4')
+          pisuks=0
+          bot.send_message(m.chat.id, 'Нахуй иди')
+        spisok2.clear()
     elif 'вирт' in p:
         bot.send_message(m.chat.id, 'Я тоже хочу повиртить!')
     elif 'хуй' in p:
