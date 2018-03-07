d5135a0c8ec022f472653895ceed30514f4ba505
egor5q <31898567+egor5q@users.noreply.github.com>
2018-03-07 20:19:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -85,6 +85,8 @@ def textm(m):
         bot.send_message(m.chat.id, speach)
     if 'п' in p and 'с' in p and 'ю' in p and 'к' in p and len(p)<250:
         pisuks=1
+        t=threading.Timer(4, pisuk)
+        t.start()
         print('3')
     if 'п' in p and 'и' in p and 'ю' in p and 'к' in p and 'д' in p and len(p)<250:
      if 'p' not in p and 'c' not in p and 'i' not in p and 'a' not in p and 'd' not in p and 'u' not in p:
