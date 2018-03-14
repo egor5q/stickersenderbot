16f65bfca07f4af0a00e6eb03cbabf299ebc9f1c
egor5q <31898567+egor5q@users.noreply.github.com>
2018-03-14 13:40:07 +0300

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -7,9 +7,21 @@ import random
 import threading
 import info
 import test
+import pprint
+from pymongo import MongoClient
 from telebot import types
 from emoji import emojize
 token = os.environ['TELEGRAM_TOKEN']
 bot = telebot.TeleBot(token)
+
+client = MongoClient('localhost', 27017)
+db = client.database
+collection = db.collection
+
+size={'x':445}
+
+sizes=db.sizes
+sizeid=sizes.insert_one(size).inserted_id
+pprint.pprint(sizes.find_one())
 
 
