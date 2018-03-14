4fb00bfe4530decf00cfb46397ae66aefc1cc52c
egor5q <31898567+egor5q@users.noreply.github.com>
2018-03-14 13:28:17 +0300

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -1,1 +1,15 @@
+# -*- coding: utf-8 -*-
+import redis
+import os
+import telebot
+import math
+import random
+import threading
+import info
+import test
+from telebot import types
+from emoji import emojize
+token = os.environ['TELEGRAM_TOKEN']
+bot = telebot.TeleBot(token)
+
 
