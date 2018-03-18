9c0b1d88b44c912c1c6944b883aa947d0762b35b
egor5q <31898567+egor5q@users.noreply.github.com>
2018-03-18 13:52:08 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,11 +32,7 @@ def sasat(m):
 
 @bot.message_handler(commands=['extend'])
 def penises(m):
-    try:
-        dick={'penis':450.0}
-        collection.save(dick)
-    except:pass
-    collection.update_one({'penis':{'$exsists:True}},{'$inc':{'penis':0.1}})
+    collection.update_one({'penis':{$exsists:True}},{'$inc':{'penis':0.1}})
     x = collection.find_one({'penis':{'$exsists':True}})
     bot.send_message(m.chat.id, 'Ура! Вы увеличили мой пенис! Теперь он '+str(x['penis'])+ 'см!')
     
