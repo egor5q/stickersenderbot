0c0757e9c741414df0af13c8353a6af79a277d43
egor5q <31898567+egor5q@users.noreply.github.com>
2018-04-21 11:50:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,6 +11,8 @@ from emoji import emojize
 token = os.environ['TELEGRAM_TOKEN']
 bot = telebot.TeleBot(token)
 
+chance=100
+
 mat=['хуй', 'пизда', 'залупа', 'хер', 'пидор', 'бля', 'ебать', 'пиздя', 'выебон', 'ебал', 'ебло', 'хуепутало', 'заебись', 'ебал', 
      'ёбаный', 'пёзда', 'хуе']
 stickerid=['CAADAgAD5QUAAnwFBxtu71sj1snukAI',
@@ -30,9 +32,31 @@ stickerid=['CAADAgAD5QUAAnwFBxtu71sj1snukAI',
 
 
 
+@bot.message_handler(commands=['setchance'])
+def getadm(m):
+     global chance
+     if m.chat.id<0:
+          x=getChatAdministrators(m.chat.id)
+          for z in x:
+          if m.from_user.id in x[z]['user_id']:
+               massiv=m.text.split('/setchance')
+               try:
+                    int(massiv[1])
+                    chance=massiv[1]
+                    bot.send_message(m.chat.id, 'Вы успешно изменили вероятность отправки стикера после мата на '+str(massiv[1])+'% !')
+               except:
+                    bot.send_message(m.chat.id, 'Неверный формат. Используйте следующий пример:\n/setchance *60*', parse_mode='markdown')
+          else:
+               bot.send_message(m.chat.id, 'Только администраторы чата могут использовать эту команду!')
+     else:
+          bot.send_message(m.chat.id, 'Эту команду можно использовать только в группе!')
+
 
 @bot.message_handler(content_types=['text'])
 def textm(m):
+  global chance
+  a=random.randint(0, 100)
+  if a<=chance:
     x=0
     for slovo in mat:
         if slovo.lower() in m.text.lower():
