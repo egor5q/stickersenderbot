5c28e4ddc4b3058e7d6e012752cd90058defb51f
egor5q <31898567+egor5q@users.noreply.github.com>
2018-04-21 11:58:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,9 +36,9 @@ stickerid=['CAADAgAD5QUAAnwFBxtu71sj1snukAI',
 def getadm(m):
      global chance
      if m.chat.id<0:
-          x=bot.get_chat_administrators(m.chat.id)
+          x=get_chat_administrators(m.chat.id)
           for z in x:
-             if m.from_user.id in x[z]['user_id']:
+             if m.from_user.id==x[z]['user_id']:
                massiv=m.text.split('/setchance')
                try:
                     int(massiv[1])
